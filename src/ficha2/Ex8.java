0fd66b0d7698e1eeb914d1f977cde7a3132ca7ae
vmcenteruk <97111683+vmcenteruk@users.noreply.github.com>
2022-02-04 10:14:56 +0000

    Ex8

--- src/ficha2/Ex8.java
+++ src/ficha2/Ex8.java
@@ -2,27 +2,44 @@ package ficha2;
 
 import java.util.Scanner;
 
-public class Ex8 {
+public class Ex8
+{
 
-		
-		public static void main(String[] args)
-		{
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Escreva um número:");
-			int numero = sc.nextInt();
+	public static void main(String[] args)
+	{
 
-			Ex8 r = new Ex8();
-			int segundos;
-			int resultado = r.convertSeconds(segundos);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insert number of seconds: ");
 
-			System.out.println("O seu fatorial (recursivamente) - " + resultado);
-			sc.close();
-		}
+		// Segundos myObj = new Segundos();
 
-		public int convertSeconds(int segundos)
-		{
-			
-		}
+		GuardaValor auxGuardaValor = new GuardaValor();
+		auxGuardaValor = convertSecondsque(sc.nextInt());
 
+		System.out.println(auxGuardaValor);
+//		auxGuardaValor.equals(myObj)
+		sc.close();
+	}
+
+	public static GuardaValor convertSecondsque(int i)
+	{
+
+		GuardaValor aux = new GuardaValor();
+		aux.set$meses(i / (60 * 60 * 24 * 30));
+		i = i % (60 * 60 * 24 * 30);
+
+		aux.set$dias(i / (60 * 60 * 24));
+		i = i % (60 * 60 * 24);
+
+		aux.set$horas(i / (60 * 60));
+		i = i % (60 * 60);
+
+		aux.set$minutos(i / 60);
+		i = i % 60;
+
+		aux.set$segundos(i);
+
+		return aux;
+	}
 
 }
